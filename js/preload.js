8eb88057b3d0b35ba8b0ead92a28d0a6a55444f7
Sam Gould <drmcman67@yahoo.com>
2018-10-09 19:35:15 -0400

    test of screen dimensions

--- js/preload.js
+++ js/preload.js
@@ -7,10 +7,11 @@ preloadState.prototype.preload = function(){
 	game.load.image("platform","assets/platform.png");
 	game.load.image("star","assets/star.png");
 	game.load.spritesheet("murph","assets/character.png",32,48);
-
+	game.load.image("telescope", "assets/TelescopeOverlay.png");
+	game.load.image("building", "assets/BuildingTest.png");
 };
 preloadState.prototype.create = function(){
-	game.state.start("Gameplay");
+	game.state.start("Telescope");
 };
 preloadState.prototype.update = function(){
 
---
+++ js/telescope.js
@@ -0,0 +1,7 @@
+let telescopeState  =function() {
+	
+};
+telescopeState.prototype.create = function(){
+	game.add(0,0,"building");
+	game.add(0,0,"telescope");
+};
