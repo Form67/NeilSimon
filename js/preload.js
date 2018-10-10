309ba5e19b94fe06c500f4300c099b67d610599c
Sam Gould <drmcman67@yahoo.com>
2018-10-10 16:43:32 -0400

    edit loaded

--- js/preload.js
+++ js/preload.js
@@ -5,6 +5,8 @@ let preloadState =function() {
 preloadState.prototype.preload = function(){
 	game.load.image("telescope", "assets/TelescopeOverlay.png");
 	game.load.image("building", "assets/BuildingTest.png");
+	game.load.image("LCurtain", "assets/NewCurtainLeft.png");
+	game.load.image("RCurtain", "assets/NewCurtainRight.png");
 };
 preloadState.prototype.create = function(){
 	game.state.start("Telescope");
