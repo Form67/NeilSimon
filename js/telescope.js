4377462a94bb3071eca839dd69be15730c929b1b
Sam Gould <drmcman67@yahoo.com>
2018-10-10 13:34:25 -0400

    Added mouse follow functionality

--- js/telescope.js
+++ js/telescope.js
@@ -2,6 +2,21 @@ let telescopeState  =function() {
 	
 };
 telescopeState.prototype.create = function(){
-	game.add(0,0,"building");
-	game.add(0,0,"telescope");
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.add.sprite(0,0,"building");
+	this.telescope = game.add.sprite(0,0,"telescope");
+	game.physics.enable(this.telescope, Phaser.Physics.ARCADE);
+	this.telescope.anchor.setTo(0.5, 0.5);
+	//game.add(0,0,"telescope");
 };
+
+telescopeState.prototype.update = function(){
+	if (game.input.mousePointer.isDown){
+		game.physics.arcade.moveToPointer(this.telescope,100000);
+		
+	}
+	else{
+		this.telescope.body.velocity.setTo(0,0);
+	}
+};
+	
