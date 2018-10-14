2cfd48f05f1918d0efb3ea38dbabebce5fdffdcc
Sam Gould <drmcman67@yahoo.com>
2018-10-14 19:07:26 -0400

    Full game with debug location enabled

--- js/telescope.js
+++ js/telescope.js
@@ -1,6 +1,7 @@
 let telescopeState  =function() {
 	this.gameProgress = 0;
 	this.gameActive = false;
+	this.gameTimer = 1;
 };
 telescopeState.prototype.create = function(){
 	this.gameActive = false;
@@ -19,13 +20,25 @@ telescopeState.prototype.create = function(){
 
 telescopeState.prototype.update = function(){
 	if(this.gameActive){
+		if(this.telescope.x >= 450 && this.telescope.x<=600 && this.telescope.y >= 450 && this.telescope.y <= 600){
+			gameTimer -= game.time.physicsElapsed;
+		}
+		else{
+			gameTimer = 3;
+		}
 		if (game.input.mousePointer.isDown){
 			this.telescope.x = game.input.x;
 			this.telescope.y = game.input.y;
 			if (this.telescope.y>1827){
 				this.telescope.y = 1827;
 			}
+			console.log(this.telescope.x + "" + this.telescope.y);
+
 			
+		}
+		if(gameTimer <= 0){
+			this.gameActive = false;
+			this.gameProgress = 3;
 		}
 	}
 	else if (this.gameProgress ===0){
@@ -40,6 +53,15 @@ telescopeState.prototype.update = function(){
 			this.gameProgress =2;
 			this.gameActive = true;
 		}
+	}
+	else if (this.gameProgress ===3){
+		this.Lcurtain.body.velocity.x = 400;
+		this.Rcurtain.body.velocity.x = -400;
+		this.gameProgress =4;
+	}
+	else if(this.gameProgress ===4 && this.Rcurtain.body.position.x <= 0){
+			this.Lcurtain.body.velocity.x = 0;
+			this.Rcurtain.body.velocity.x = 0;
 	}
 
 	
