fbbd76e264ec6e7b82c739b61c0ac2fa305b7b70
Unknown <thompz@rpi.edu>
2018-10-17 12:52:04 -0400

    Sounds

--- js/preload.js
+++ js/preload.js
@@ -27,12 +27,12 @@ preloadState.prototype.preload = function(){
 	game.load.bitmapFont("carrier_command", "assets/carrier_command.png", "assets/carrier_command.xml");
 	game.load.image("LCurtain", "assets/NewCurtainLeft.png");
 	game.load.image("RCurtain", "assets/NewCurtainRight.png");
-	game.load.audio("gunBreak", "");
+	game.load.audio("gunBreak", "assets/sounds/gun_break.mp3");
 	game.load.audio("gunDrag", "");
-	game.load.audio("gunSnap", "assets/sounds/gun_snap");
-	game.load.audio("typing", "");
+	game.load.audio("gunSnap", "assets/sounds/gun_snap.mp3");
+	game.load.audio("typing", "assets/sounds/typing.mp3");
 	game.load.audio("bat", "");
-	game.load.audio("moan", "");
+	game.load.audio("moan", "assets/sounds/moan.mp3");
 	game.load.image("end","assets/EndScreen.png");
 	//help please
 	//game.load.bitmapFont("Typewriter", "assets/carrier_command.png", "assets/carrier_command.xml");
